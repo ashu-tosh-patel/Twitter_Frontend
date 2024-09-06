c54efd08d2ae46e82545686249e3fa093bb594c1
ashutosh.patel <ashutosh.patel@infosys.com>
2024-09-06 16:46:43 +0530

    Initialized basiz components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ExploreBarComponent } from './explore-bar/explore-bar.component';
+import { FeedComponent } from './feed/feed.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    SearchBarComponent,
+    ExploreBarComponent,
+    FeedComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/explore-bar/explore-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explore-bar',
+  templateUrl: './explore-bar.component.html',
+  styleUrls: ['./explore-bar.component.css']
+})
+export class ExploreBarComponent {
+
+}

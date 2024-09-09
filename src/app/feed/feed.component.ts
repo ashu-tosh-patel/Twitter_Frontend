1d13d9cf6807e1a0ff596e2aa5008dfe98602923
sachin.arora05 <sachin.arora05@infosys.com>
2024-09-09 14:33:15 +0530

    Search User implemented now just need to navigate it to user profile

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FeedService } from './feed.service';
+import { userData } from '../tweet-component/userData';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent {
+export class FeedComponent implements OnInit{
 
+  followingList: number[] = []
+  usersList: userData[] = []
+  usersProfileForFeed : userData[] = []
+  errorFollowingApi !: any
+  errorUserApi !: any
+  userId !: number
+
+  constructor(private feedService: FeedService){}
+
+  // Fetch userId of user from After successfull Login
+
+  ngOnInit(): void {
+    this.getFollowingList();
+    this.getUsersList();
+  }
+
+  getFollowingList(){
+    this.feedService.getFollowingIdApi(this.userId).subscribe(
+      list => this.followingList = list,
+      error => this.errorFollowingApi = error
+    )
+  }
+
+  getUsersList(){
+    this.feedService.getUsersApi().subscribe(
+      (list : userData[]) => {
+        this.usersList = list,
+        this.usersProfileForFeed = this.usersList.filter((x) => this.followingList.includes(x.id))
+        console.log('Data received of Users',this.usersProfileForFeed)
+      },
+      (error) => {
+        this.errorUserApi = error
+      }
+    )
+  }
 }
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { SearchService } from './search.service';
+import { Router } from '@angular/router';
+import { debounceTime, switchMap } from 'rxjs/operators'
+import { tweetData } from '../tweet-component/tweetData';
+import { userData } from './userData';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit{
+  searchControl = new FormControl('')
+  suggestions$: Observable<any[]> = of([])
+
+  constructor(private searchService: SearchService, private router: Router){}
+
+  ngOnInit(): void {
+    this.getUserDetails()
+    this.suggestions$ = this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      switchMap(value => this.getSuggestions(value))
+    )
+  }
+  allSuggestions : userData[] = [] 
+  errorMessage !: any 
+
+  showclickedUser(value: number){
+    this.router.navigate(['user-profile',value])
+  }
+
+  getSuggestions(query: any): Observable<any[]>{
+    if(!query.trim()){
+      return of([]);
+    }
+    const filteredSuggestions = this.allSuggestions.filter(item =>
+      item.name.toLowerCase().includes(query.toLowerCase()) ||
+      item.username.toLowerCase().includes(query.toLowerCase()) ||
+      item.bio.toLowerCase().includes(query.toLowerCase())
+    )
+    return of(filteredSuggestions)
+  }
+
+  getUserDetails(){
+    this.searchService.getUserDetailsApi().subscribe(
+      (data: userData[]) => {
+        this.allSuggestions = data;
+        console.log('Data recv',this.allSuggestions)
+      },
+      (error) => {
+        this.errorMessage = error
+      }
+    )
+  }
 
 }
